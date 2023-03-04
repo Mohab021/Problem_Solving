b59ad5583942b6066cbf2ce549bcc94a8efd27ad
Mohab Sharafeldin <mohabosamah2011@yahoo.com>
2023-03-04 21:24:49 +0200

    second commit

--- PS_main/PS_main.cpp
+++ PS_main/PS_main.cpp
@@ -1,23 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//void setup();
 int main()
 {
-    //setup();
-
     int a, b;
     cin >> a >> b;
     cout << a+b;
 
     return 0;
 }
-
-/*void setup()
-{
-    ios_base::sync_with_stdio(false);
-#ifndef ONLINE_JUDGE
-    freopen("test.in.txt", "rt", stdin);
-    freopen("s.out.txt", "wt", stdout);
-#endif // ONLINE_JUDGE
-}*/
